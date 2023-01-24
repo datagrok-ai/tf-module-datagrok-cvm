3542a88a5e1d6e147ddbbd4f4e77c9e14c9f10d6
Dmytro Nahovskyi <dnahovskyi@datagrok.ai>
2023-01-24 17:01:06 +0200

    GROK-11188-add h20 to lb int

--- aws/lb.tf
+++ aws/lb.tf
@@ -61,6 +61,20 @@ module "lb_int_sg" {
       description = "Access to HTTP"
       cidr_blocks = try(module.vpc[0].vpc_cidr_block, var.cidr)
     },
+    {
+      from_port   = 54321
+      to_port     = 54321
+      protocol    = "tcp"
+      description = "Access to h20"
+      cidr_blocks = try(module.vpc[0].vpc_cidr_block, var.cidr)
+    },
+    {
+      from_port   = 5005
+      to_port     = 5005
+      protocol    = "tcp"
+      description = "Access to h20h"
+      cidr_blocks = try(module.vpc[0].vpc_cidr_block, var.cidr)
+    },
   ]
 }
 resource "aws_route53_zone" "external" {
