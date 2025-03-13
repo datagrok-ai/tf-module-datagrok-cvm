eec1b9c913435ad40e1cc004521de90527d9869d
VHlushchen <VHlushchen@users.noreply.github.com>
2025-03-13 11:07:40 +0000

    GitHub Actions: Refactor: Automated formatting of terraform code

--- aws/data.tf
+++ aws/data.tf
@@ -10,15 +10,15 @@ resource "aws_ssm_parameter" "grok_parameters" {
     {
       jupyterToken : jsondecode(data.aws_secretsmanager_secret_version.jkg_secret.secret_string)["token"]
       capabilities : ["jupyter"]
-      isolatesCount: var.jkgIsolatesCount,
+      isolatesCount : var.jkgIsolatesCount,
       queuePluginSettings = {
-        amqpHost     = split(":", split("://", data.aws_mq_broker.name.instances[0].endpoints[0])[1])[0]
-        amqpPassword = var.rabbitmq_password
-        amqpPort     = var.amqpPort
-        amqpUser     = var.rabbitmq_username
-        tls = var.amqpTLS
-        pipeHost     = "grok_pipe.datagrok.${split("-",var.name)[0]}.${var.environment}.internal:3000"
-        pipeKey      = var.pipeKey
+        amqpHost           = split(":", split("://", data.aws_mq_broker.name.instances[0].endpoints[0])[1])[0]
+        amqpPassword       = var.rabbitmq_password
+        amqpPort           = var.amqpPort
+        amqpUser           = var.rabbitmq_username
+        tls                = var.amqpTLS
+        pipeHost           = "grok_pipe.datagrok.${split("-", var.name)[0]}.${var.environment}.internal:3000"
+        pipeKey            = var.pipeKey
         maxConcurrentCalls = 4
 
       }
