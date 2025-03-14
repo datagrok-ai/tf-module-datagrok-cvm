3863afea87de373bfa861db38ee4fde885350a8b
Vladyslav Hlushchenko <vhlushchenko@datagrok.ai>
2025-03-14 12:53:26 +0200

    JN: removed

--- aws/ecs.tf
+++ aws/ecs.tf
@@ -297,18 +297,18 @@ resource "aws_ecs_task_definition" "jkg" {
             {
               jupyterToken : jsondecode(data.aws_secretsmanager_secret_version.jkg_secret.secret_string)["token"]
               capabilities : ["jupyter"]
-              isolatesCount: var.jkgIsolatesCount,
+              isolatesCount : var.jkgIsolatesCount,
               queuePluginSettings = {
-                amqpHost = "rabbitmq.datagrok-${var.environment}.local"
-                amqpPassword = var.rabbitmq_password
-                amqpPort     = var.amqpPort
-                amqpUser     = var.rabbitmq_username
-                tls = var.amqpTLS
-                pipeHost     = "grok_pipe.datagrok.${split("-",var.name)[0]}.${var.environment}.internal"
-                pipeKey      = var.pipeKey
+                amqpHost           = "rabbitmq.datagrok-${var.environment}.local"
+                amqpPassword       = var.rabbitmq_password
+                amqpPort           = var.amqpPort
+                amqpUser           = var.rabbitmq_username
+                tls                = var.amqpTLS
+                pipeHost           = "grok_pipe.datagrok.${split("-", var.name)[0]}.${var.environment}.internal"
+                pipeKey            = var.pipeKey
                 maxConcurrentCalls = 4
               }
-            })
+          })
         }
       ]
       logConfiguration = {
